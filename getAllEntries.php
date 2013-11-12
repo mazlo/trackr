a2b0af34c7ee27efffc4d711e396b3351d68d688
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-11-12 23:23:18 +0100

    extracted displaying all the entries to separate file: getAllEntries.php. Is invoked on document ready

---
+++ getAllEntries.php
@@ -0,0 +1,67 @@
+<?php
+
+	// connect
+	$mysqli = new mysqli( "localhost", "root", "root", "shorter");
+
+	if ( $mysqli->connect_errno ) 
+	{
+	    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
+	// get all entries
+    $result = $mysqli->query( 'SELECT id,title,description FROM entry ORDER BY id DESC' );
+
+	while( $row = $result->fetch_assoc() ) { ?>
+
+	<!-- wrapper for one entry -->
+	<div class="wrapper_entry">
+
+		<a href='#' class='entry_delete_link' eid='<? echo $row['id']; ?>' style='display: block; padding: 3px 13px'>-</a>
+
+		<div class='entry'>
+			<h4 class='entry_title'>#<? echo $row['id'] . " " . $row['title']; ?></h4>
+			<h4 class='entry_description'><? echo $row['description']; ?></h4>
+			
+			<a href="#" class="div_comment_add" eid="<? echo $row['id']; ?>">+</a>
+
+			<?
+			$result2 = $mysqli->query( 'SELECT comment, date FROM comment WHERE entry_id = '. $row['id'] .' ORDER BY id DESC' );
+
+			if ( $result2 ) { ?>
+
+				<!-- wrapper for comment dialog -->
+				<div class='wrapper_comments'>
+					<h4 class='entry_title' style='margin: 6px 0;'>Comments</h4>
+
+					<!-- div comment add: is hidden first -->
+					<div id="div_comment_add_<? echo $row['id']; ?>" style="display: none;">
+						<textarea id="comment_new_content_<? echo $row['id']; ?>"></textarea>
+
+						<div style="margin: 8px; 0">
+							<input type="button" class="comment_add_button" eid="<? echo $row['id']; ?>" value="Add" />
+							<input type="button" class="comment_add_cancel" eid="<? echo $row['id']; ?>" value="Cancel" />
+						</div>
+					</div>
+					
+					<!--  wrapper for all comments of an entry -->
+					<div id="comments_<? echo $row['id']; ?>">
+				<?
+				while( $comments = $result2->fetch_assoc() ) { ?>		
+						<p class='comment' style='margin: 0; padding: 6px 13px;'>
+							<? echo $comments['comment']; ?>
+						</p>
+			<? 	} 
+
+				$result2->close(); ?>
+					</div>
+				</div>
+		<? 	} ?>
+		</div>
+	</div>
+	<?php
+	}
+	
+	$result->close();
+	$mysqli->close();
+
+	?>
--- index.php
+++ index.php
@@ -67,58 +67,7 @@
 
 			<!-- list of entries -->
 			<div id="entries">
-				<?php
-
-				while( $row = mysql_fetch_array( $result ) ) { ?>
-
-				<!-- wrapper for one entry -->
-				<div class="wrapper_entry">
-
-					<a href='#' class='entry_delete_link' eid='<? echo $row['id']; ?>' style='display: block; padding: 3px 13px'>-</a>
-
-					<div class='entry'>
-						<h4 class='entry_title'>#<? echo $row['id'] . " " . $row['title']; ?></h4>
-						<h4 class='entry_description'><? echo $row['description']; ?></h4>
-						
-						<a href="#" class="div_comment_add" eid="<? echo $row['id']; ?>">+</a>
-
-						<?
-						$result2 = mysql_query( 'SELECT comment, date FROM comment WHERE entry_id = '. $row['id'] .' ORDER BY id DESC' );
-
-						if ( $result2 ) { ?>
-
-						<!-- wrapper for comment dialog -->
-						<div class='wrapper_comments'>
-							<h4 class='entry_title' style='margin: 6px 0;'>Comments</h4>
-
-							<!-- div comment add: is hidden first -->
-							<div id="div_comment_add_<? echo $row['id']; ?>" style="display: none;">
-								<textarea id="comment_new_content_<? echo $row['id']; ?>"></textarea>
-
-								<div style="margin: 8px; 0">
-									<input type="button" class="comment_add_button" eid="<? echo $row['id']; ?>" value="Add" />
-									<input type="button" class="comment_add_cancel" eid="<? echo $row['id']; ?>" value="Cancel" />
-								</div>
-							</div>
-							
-							<!--  wrapper for all comments of an entry -->
-							<div id="comments_<? echo $row['id']; ?>">
-						<?
-						while( $comments = mysql_fetch_array( $result2 ) ) { ?>		
-								<p class='comment' style='margin: 0; padding: 6px 13px;'>
-									<? echo $comments['comment']; ?>
-								</p>
-					<? 	} ?>
-							</div>
-						</div>
-					<? 	} ?>
-					</div>
-				</div>
-			<?php
-				}
-				
-				mysql_close( $link );
-			?>
+				<!-- ajax response here -->
 			</div>
 		</div>
 
@@ -128,6 +77,25 @@
 	</body>
 
 	<script type="text/javascript">
+
+		// on document ready load all entries
+		$jQ( function()
+		{
+			getAllEntries();
+		});
+
+		var getAllEntries = function()
+		{
+			$jQ.ajax( {
+				url: "getAllEntries.php",
+				type: "get",
+
+				success: function( data ) 
+				{
+					$jQ( '#entries' ).html( data );
+				}
+			});
+		};
 
 		// shows the div to add a new entry
 		$jQ(".div_entry_add").click( function() 
