da0ef5708497bcb13b178273edcf8f627023179f
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-12-08 19:06:59 +0100

    increased text areas

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -15,10 +15,10 @@
         <input name='_token' type='hidden' value='TbVUNXU82jmVLLrSGHT360dEYlhRmX5ca0E1iPxv'>
 
         <h4 class='normal'>Username</h4>
-        <input placeholder='john.smith' name='username' type='text' id='username' class='textfield_smaller'>
+        <input placeholder='john.smith' name='username' type='text' id='username' class='textfield' style='width: 300px'>
         
         <h4 class='normal'>Password</h4>
-        <input placeholder='●●●●●●●●●●' name='password' type='password' value='' id='password' class='textfield_smaller'>
+        <input placeholder='●●●●●●●●●●' name='password' type='password' value='' id='password' class='textfield' style='width: 300px'>
 
         <input type='submit' value='login' id='login_button' class='button' style='display: block; margin: 23px 0 0'>
 
