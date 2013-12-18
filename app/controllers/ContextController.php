4325ed537d2625ebd994e72fe3addd824bf8822e
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-12-18 23:14:42 +0100

    load all Contexts for user

--- app/controllers/ContextController.php
+++ app/controllers/ContextController.php
@@ -2,9 +2,12 @@
 
 class ContextController extends BaseController {
 
+	/*
+		Loads all contexts for current user
+	*/
 	public function showAll()
 	{
-		$contexts = Context::all();
+		$contexts = User::find( Auth::user()->id )->contexts()->get();
 
 		return View::make( 'contexts' )->with( 'contexts', $contexts );
 	}
