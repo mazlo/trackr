e55804cb19dd241c58a84bfe6a972cb09762c150
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-08 01:34:20 +0100

    added first draft of feature list that should like to mindstackr image

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -35,16 +35,33 @@
 @stop
 
 @section( 'footer' )
-
     <div style='padding: 23px;'>
-        <h3>What's a <span style='font-size: 23px'>M</span>ind<span style='font-size: 23px'>S</span>tackr?</h3>
 
-        <p>MindStackr helps you organize your minds in terms of stacks, which basically is a list of items. Each item in the list we call a MindStack.
-            It is called a MindStack, because when an item of a specific topic is created you can comment on it and that way track a history of thoughts concerning your topic.</p>
+        <h3>What the heck is <span style='font-size: 23px'>M</span>ind<span style='font-size: 23px'>S</span>tackr?</h3>
+
+        <p>MindStackr helps you organize your minds in terms of stacks, which basically is a list of items. 
+            Each item in the list we call a Stack, which usuablly covers a specific topic and it consists of no more or less 
+            a title and a description.
+            The fundamental idea behind a Stack is that you can comment on it and that way track a history of thoughts 
+            concerning your topic.
+        </p>
 
         <h3>MindStackr is laid-out with maximum usability in mind.</h3>
 
-        <p></p>
+        <div style=''>
+            <ul style='overflow: auto; list-style-type: none; padding: 8px 0 0;'>
+                <li style='float: left; background: #fcfcfc; border: 1px #bbb solid; width: 145px; height: 105px; padding: 13px; margin: 0 13px;'>
+                    Change the title of a Stackr or the Comments section and confirm by clicking enter.
+                </li>
+
+                <li style='float: left; background: #fcfcfc; border: 1px #bbb solid; width: 145px; height: 105px; padding: 13px; margin: 0 13px;'>
+                    Organize the order of Comments by dragging.
+                </li>
+            </ul>
+        </div>
+
+        <div style='clear: both'><img src='{{ url("resources/mindstackr_screenshot.png") }}' /></div>
+
     </div>
 
 @stop
