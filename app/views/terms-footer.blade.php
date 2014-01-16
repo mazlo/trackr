a733cf6a911ee388a0388b222e8cba284301eb73
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-16 22:52:56 +0100

    renamed terms-footer -> terms-section

--- app/views/terms-footer.blade.php
+++
@@ -1,8 +0,0 @@
-
-<div id='terms-content'>
-    <p class='terms-wrapper'>
-        <span>Copyright by <a class='terms' href='mindstackr.com'>MindStackr.com</a></span>
-        <a class='terms' href='terms-and-conditions'>Terms and Conditions</a>
-        <a class='terms' href='feedback'>Give us feedback</a>
-    </p>
-</div>
