0c179aeae1928a073070e39be9c25b8fae5c2706
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-16 21:58:27 +0100

    added terms and conditions text and formatted it

---
+++ app/views/terms.blade.php
@@ -0,0 +1,234 @@
+@extends( 'layout' )
+
+@section( 'beforeContent' )
+
+<h3>Website Terms and Conditions</h3>
+
+@stop
+
+@section( 'content' )
+
+<div id='terms-wrapper'>
+
+	<h4>Credit</h4>
+
+	<p>
+		This document was created using a Contractology template available at http://www.freenetlaw.com.
+	</p>
+
+	<h4>Introduction</h4>
+
+	<p>
+		These terms and conditions govern your use of this website; by using this website, you accept these terms and conditions 
+		in full. If you disagree with these terms and conditions or any part of these terms and conditions, you must not use this 
+		website. 
+	</p>
+
+	<h4>License to Use Website</h4>
+
+	<p>
+		Unless otherwise stated, MindStackr.com and/or its licensors own the intellectual property rights in the website and material 
+		on the website. Subject to the license below, all these intellectual property rights are reserved.
+	</p>
+
+	<h4>Acceptable Use</h4>
+
+	<p>
+		You must not use this website in any way that causes, or may cause, damage to the website or impairment of the availability 
+		or accessibility of the website; or in any way which is unlawful, illegal, fraudulent or harmful, or in connection with any 
+		unlawful, illegal, fraudulent or harmful purpose or activity.
+	</p>
+
+	<p>
+		You must not use this website to copy, store, host, transmit, send, use, publish or distribute any material which consists 
+		of (or is linked to) any spyware, computer virus, Trojan horse, worm, keystroke logger, rootkit or other malicious computer 
+		software.
+	</p>
+
+	<p>
+		You must not conduct any systematic or automated data collection activities (including without limitation scraping, data 
+		mining, data extraction and data harvesting) on or in relation to this website without MindStackr.com's express written 
+		consent.
+	</p>
+
+	<h4>Restricted Access</h4>
+
+	<p>
+		Access to certain areas of this website is restricted. MindStackr.com reserves the right to restrict access to areas of 
+		this website, or indeed this entire website, at MindStackr.com's discretion.
+	</p>
+
+	<p>
+		If MindStackr.com provides you with a user ID and password to enable you to access restricted areas of this website or 
+		other content or services, you must ensure that the user ID and password are kept confidential.  
+	</p>
+
+	<h4>User Content</h4>
+
+	<p>
+		In these terms and conditions, “your user content” means material (including without limitation text, images, audio material, 
+		video material and audio-visual material) that you submit to this website, for whatever purpose.
+	</p>
+
+	<p>
+		Your user content must not be illegal or unlawful, must not infringe any third party's legal rights, and must not be capable 
+		of giving rise to legal action whether against you or MindStackr.com or a third party (in each case under any applicable law).  
+	</p>
+
+	<p>
+		You must not submit any user content to the website that is or has ever been the subject of any threatened or actual legal 
+		proceedings or other similar complaint.
+	</p>
+
+	<p>
+		MindStackr.com reserves the right to edit or remove any material submitted to this website, or stored on MindStackr.com's 
+		servers, or hosted or published upon this website.
+	</p>
+
+	<h4>No Warranties</h4>
+
+	<p>
+		This website is provided “as is” without any representations or warranties, express or implied. MindStackr.com makes no 
+		representations or warranties in relation to this website or the information and materials provided on this website.  
+	</p>
+
+	<p>
+		Without prejudice to the generality of the foregoing paragraph, MindStackr.com does not warrant that:
+
+		<ul>
+			<li>this website will be constantly available, or available at all; or</li>
+			<li>the information on this website is complete, true, accurate or non-misleading.</li>
+		</ul>
+
+		Nothing on this website constitutes, or is meant to constitute, advice of any kind.
+	</p>
+
+	<h4>Limitations of Liability</h4>
+
+	<p>
+		MindStackr.com will not be liable to you (whether under the law of contact, the law of torts or otherwise) in relation to 
+		the contents of, or use of, or otherwise in connection with, this website:
+
+		<ul>
+			<li>to the extent that the website is provided free-of-charge, for any direct loss;</li>
+			<li>for any indirect, special or consequential loss; or</li>
+			<li>for any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business 
+				relationships, loss of reputation or goodwill, or loss or corruption of information or data.</li>
+		</ul>
+
+		These limitations of liability apply even if MindStackr.com has been expressly advised of the potential loss.
+	</p>
+
+	<h4>Exceptions</h4>
+
+	<p>
+		Nothing in this website disclaimer will exclude or limit any warranty implied by law that it would be unlawful to exclude or 
+		limit; and nothing in this website disclaimer will exclude or limit MindStackr.com's liability in respect of any:
+
+		<ul>
+			<li>death or personal injury caused by MindStackr.com' negligence;</li>
+			<li>fraud or fraudulent misrepresentation on the part of MindStackr.com; or</li>
+			<li>matter which it would be illegal or unlawful for MindStackr.com to exclude or limit, or to attempt or purport to 
+				exclude or limit, its liability. </li>
+		</ul>
+
+	<h4>Reasonableness</h4>
+
+	<p>
+		By using this website, you agree that the exclusions and limitations of liability set out in this website disclaimer are 
+		reasonable.
+	</p>
+
+	<p>
+		If you do not think they are reasonable, you must not use this website.
+	</p>
+
+	<h4>Unenforceable Provisions</h4>
+
+	<p>
+		If any provision of this website disclaimer is, or is found to be, unenforceable under applicable law, that will not affect 
+		the enforceability of the other provisions of this website disclaimer.
+	</p>
+
+	<h4>Indemnity</h4>
+
+	<p>
+		You hereby indemnify MindStackr.com and undertake to keep MindStackr.com indemnified against any losses, damages, costs, 
+		liabilities and expenses (including without limitation legal expenses and any amounts paid by MindStackr.com to a third 
+		party in settlement of a claim or dispute on the advice of MindStackr.com's legal advisers) incurred or suffered by 
+		MindStackr.com arising out of any breach by you of any provision of these terms and conditions[, or arising out of any 
+		claim that you have breached any provision of these terms and conditions.
+	</p>
+
+	<h4>Breaches of These Terms and Conditions</h4>
+
+	<p>
+		Without prejudice to MindStackr.com's other rights under these terms and conditions, if you breach these terms and conditions 
+		in any way, MindStackr.com may take such action as MindStackr.com deems appropriate to deal with the breach, including 
+		suspending your access to the website, prohibiting you from accessing the website, blocking computers using your IP 
+		address from accessing the website, contacting your internet service provider to request that they block your access to the 
+		website and/or bringing court proceedings against you.
+	</p>
+
+	<h4>Variation</h4>
+
+	<p>
+		MindStackr.com may revise these terms and conditions from time-to-time.  Revised terms and conditions will apply to the use 
+		of this website from the date of the publication of the revised terms and conditions on this website.  Please check this 
+		page regularly to ensure you are familiar with the current version.
+	</p>
+
+	<h4>Assignment</h4>
+
+	<p>
+		MindStackr.com may transfer, sub-contract or otherwise deal with MindStackr.com's rights and/or obligations under these terms 
+		and conditions without notifying you or obtaining your consent.
+	</p>
+
+	<p>
+		You may not transfer, sub-contract or otherwise deal with your rights and/or obligations under these terms and conditions.
+	</p>
+
+	<h4>Severability</h4>
+
+	<p>
+		If a provision of these terms and conditions is determined by any court or other competent authority to be unlawful and/or 
+		unenforceable, the other provisions will continue in effect.  If any unlawful and/or unenforceable provision would be lawful 
+		or enforceable if part of it were deleted, that part will be deemed to be deleted, and the rest of the provision will 
+		continue in effect. 
+	</p>
+
+	<h4>Entire Agreement</h4>
+
+	<p>
+		These terms and conditions constitute the entire agreement between you and MindStackr.com in relation to your use of this 
+		website, and supersede all previous agreements in respect of your use of this website.
+	</p>
+
+	<h4>MindStackr.com's Details</h4>
+
+	<p>
+		MindStackr.com is not a registered company name. It is the name of the service and the website.
+	</p>
+
+	<p>
+		MindStackr.com was programmed by Matthäus Zloch, matthaeus@mazlo.de
+	</p>
+
+	<p>
+		You can contact MindStackr.com by email to info@mindstackr.com.
+	</p>
+
+</div>
+
+@stop
+
+@section( 'terms' )
+    <div id='terms-content'>
+        <p class='terms-wrapper'>
+            <span>Copyright by MindStackr.com</span>
+            <a class='terms' href='terms-and-conditions'>Terms and Conditions</a>
+        </p>
+    </div>
+@stop
+
