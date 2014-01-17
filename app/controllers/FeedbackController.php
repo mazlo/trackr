78042334c77c7f96790618a0cb81a2f836ad6ebc
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-17 18:13:11 +0100

    persists a feedback submission

--- app/controllers/FeedbackController.php
+++ app/controllers/FeedbackController.php
@@ -16,6 +16,21 @@ class FeedbackController extends BaseController
 	*/
 	public function add()
 	{
+		$feedback = new Feedback();
 
+		$feedback->feedbackType = Input::get( 'feedbackType' );
+		$feedback->reason = Input::get( 'reason' );
+
+		$comment = Input::get( 'comment' );
+		if ( !empty( $comment ) )
+			$feedback->comment = $comment;
+
+		$emailTo = Input::get( 'emailTo' );
+		if ( !empty( $emailTo ) )
+			$feedback->emailTo = $emailTo;
+
+		$feedback->save();
+
+		return View::make( 'feedback' )->with( 'submitted', true );
 	}
 }
--- app/views/feedback.blade.php
+++ app/views/feedback.blade.php
@@ -8,7 +8,12 @@
 
 @section( 'content' )
 
-	<h4 class='normal'>Thank you for using MindStackr.com. We appreciate your feedback!</h4>
+	@if ( isset( $submitted ) && $submitted )
+		<h4 style='color: #ba0000'>Thank you for your feedback! We're doing our best.</h4>
+		<h4>You might want to say something else?</h4>
+	@else
+		<h4>Thank you for using MindStackr.com. We appreciate your feedback!</h4>
+	@endif
 
 	<form method='post' action='{{ url("feedback") }}' accept-charset='UTF-8' autocomplete='off' class='form-default form-feedback' style='width: 460px'>
 
@@ -16,25 +21,25 @@
 
 		<h4>What kind of feedback do you want to give?</h4>
 
-		<select>
+		<select name='feedbackType'>
 			<option>I just wanted to say that I like it</option>
 			<option>Sorry, not my thing</option>
 		</select>
 
 		<h4>It there a specific reason?</h4>
 
-		<select>
+		<select name='reason'>
 				<option>Just so</option>
 				<option>Yeah, but I'll not tell ya!</option>
 		</select>
 
 		<h4>What else do you want to say or ask?</h4>
 
-        <textarea class='textarea form' placeholder='Your opinion (optional)'></textarea>
+        <textarea class='textarea form' name='comment' placeholder='Your opinion (optional)'></textarea>
         
         <h4>Please send an answer to (or a comment on) my feedback to</h4>
 
-		<input placeholder='john.smith@email.com' name='email' type='text' id='email' class='textfield form'>
+		<input placeholder='john.smith@email.com' name='emailTo' type='text' id='emailTo' class='textfield form'>
 
         <input type='submit' value='send feedback' id='login_button' class='button login-button' style='width: 110px;'>
 
