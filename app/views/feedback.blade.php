99d295bc5bc343d07c802f5cd1eb9495071216a7
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-17 08:31:24 +0100

    added rudimentary feedback form

--- app/views/feedback.blade.php
+++ app/views/feedback.blade.php
@@ -10,6 +10,36 @@
 
 	<h4 class='normal'>Thank you for using MindStackr.com. We appreciate your feedback!</h4>
 
+	<form method='post' action='{{ url("feedback") }}' accept-charset='UTF-8' autocomplete='off' class='form-default form-feedback' style='width: 460px'>
+
+		<h3>Feedback Form</h3>
+
+		<h4>What kind of feedback do you want to give?</h4>
+
+		<select>
+			<option>I just wanted to say that I like it</option>
+			<option>Sorry, not my thing</option>
+		</select>
+
+		<h4>It there a specific reason?</h4>
+
+		<select>
+				<option>Just so</option>
+				<option>Yeah, but I'll not tell ya!</option>
+		</select>
+
+		<h4>What else do you want to say or ask?</h4>
+
+        <textarea class='textarea form' placeholder='Your opinion (optional)'></textarea>
+        
+        <h4>Please send an answer to (or a comment on) my feedback to</h4>
+
+		<input placeholder='john.smith@email.com' name='email' type='text' id='email' class='textfield form'>
+
+        <input type='submit' value='send feedback' id='login_button' class='button login-button' style='width: 110px;'>
+
+	</form>
+
 @stop
 
 @section( 'terms' )
