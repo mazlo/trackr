cab54f0e6e63dc49386e6f6d8ac88dbb0451139a
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-17 08:07:17 +0100

    added url() function to links

---
+++ app/views/terms-section.blade.php
@@ -0,0 +1,8 @@
+
+<div id='terms-content'>
+    <p class='terms-wrapper'>
+        <span>Copyright by <a class='terms' href='http://mindstackr.com'>MindStackr.com</a></span>
+        <a class='terms' href='{{ url("terms-and-conditions") }}'>Terms and Conditions</a>
+        <a class='terms' href='{{ url("feedback") }}'>Give us feedback</a>
+    </p>
+</div>
