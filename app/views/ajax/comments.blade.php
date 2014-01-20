e67115006da1512f83fe8e58ae9b3ffa8ef9a2ad
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-01-20 23:56:31 +0100

    removed display: table-cell on Comments

--- app/views/ajax/comments.blade.php
+++ app/views/ajax/comments.blade.php
@@ -11,8 +11,8 @@
 
 @foreach( $comments as $comment )
 	<li class='comment' cid='{{ $comment->id }}'>
-		<span style='display: table-cell;'><a href='#' class='comment_delete_link' cid='{{ $comment->id }}'>-</a></span>
-		<span style='display: table-cell;' class='searchable'>{{ $comment->comment }}</span>
+		<span><a href='#' class='comment_delete_link' cid='{{ $comment->id }}'>-</a></span>
+		<span class='searchable'>{{ $comment->comment }}</span>
 		<span class='comment_delete_confirmation' eid='{{ $stackr->id }}' cid='{{ $comment->id }}'>
 			<button class='operator-button operator-button-confirm'>delete</button>
 		</span>
