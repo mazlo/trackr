1ec29effcbee12fdc787a0d47cea074e8bde5184
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-02-18 23:42:27 +0100

    removed x-button from context-options-box -> introduced context-header-box

--- app/views/ajax/contexts.blade.php
+++ app/views/ajax/contexts.blade.php
@@ -15,6 +15,14 @@
 @foreach( $contexts as $context )
 	
 	<li class='context-wrapper' cname='{{ $context->name }}'>
+
+		<div class='context-header-box element-hoverable'>
+			<button class='operator-button operator-button-narrow operator-button-vertical context-delete-link'>x</button>
+			<span class='context-delete-confirm'>
+				<button class='operator-button operator-button-confirm' style='width: auto'>delete</button>
+			</span>
+		</div>
+		
 		<div class='context element-shadow' style='background: {{ $context->color }}'>	
 			<a href='{{ url( "contexts/$context->name/stackrs" ) }}'>
 				{{ $context->name }}
@@ -37,10 +45,6 @@
 		</div>
 
 		<div class='context-options-box element-hoverable'>
-			<button class='operator-button operator-button-narrow operator-button-vertical context-delete-link' style='margin-left: -16px; margin-top: -10px'>x</button>
-			<span class='context-delete-confirm'>
-				<button class='operator-button operator-button-confirm' style='width: auto'>delete</button>
-			</span>
 
 			@foreach( $colors[ $context->name ] as $contextColors )
 				<button class='operator-button operator-button-narrow operator-button-vertical context_color_button' style='background: {{ $contextColors->color }}' color='{{ $contextColors->color }}'></button>
