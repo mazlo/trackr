423af44692ee9f8f646909c59e3786e262e06623
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-02-27 15:43:15 +0100

    added rsync script to sync all required files

--- bin/sync.production.controllers.sh
+++ bin/sync.production.controllers.sh
@@ -1,2 +1,2 @@
 # syncing controllers
-rsync --delete -avze ssh app/controllers mindstackr.com@ssh.mindstackr.com:/www/app/
+rsync --delete -avze ssh app/routes.php app/controllers mindstackr.com@ssh.mindstackr.com:/www/app/
