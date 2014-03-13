eea7e596b15d2559e4f8de384fe366d22f560502
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 20:16:54 +0100

    added Stackr.title and Stackr.description to tooltip

--- app/views/ajax/snippets/relatedTo.blade.php
+++ app/views/ajax/snippets/relatedTo.blade.php
@@ -3,4 +3,4 @@
 	$relatedStackrUrl = url( '/contexts/' . $relatedStackr->context->name . '/stackrs#' . $relatedStackr->id ); 
 ?>
 
-<a class='dotted element-tooltip' href='{{ $relatedStackrUrl }}' title='{{ $relatedStackr->title }}'>related to #{{ $stackr->relatedTo }}</a>
+<a class='dotted element-tooltip' href='{{ $relatedStackrUrl }}' title='<h3>{{ e( $relatedStackr->title ) }}</h3><span>{{ e( $relatedStackr->description ) }}</span>''>related to #{{ $stackr->relatedTo }}</a>
