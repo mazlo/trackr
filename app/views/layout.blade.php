82cf0b6f20ba5f80a9941a8556a1412814274a60
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 22:20:20 +0100

    doctype constraint: removed h2 from within an a element

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -48,7 +48,7 @@
 				<div style='width: 25%'>
 					<a href='{{ URL::to( "contexts" ) }}'>
 						<img src='{{ url( "resources/stack.png" ) }}' style='width: 32px; margin-bottom: -4px;' />
-						<h2 class='applicationTitle'><span style='font-size: 32px'>M</span>ind<span style='font-size: 32px'>S</span>tackr<span style='position: absolute; top: 22px; border-radius: 3px; background-color: pink; margin-left: 13px; padding: 4px 8px; font-size: 14px;'>beta</span></h2>
+						<span class='applicationTitle'><span style='font-size: 32px'>M</span>ind<span style='font-size: 32px'>S</span>tackr<span style='position: absolute; top: 22px; border-radius: 3px; background-color: pink; margin-left: 13px; padding: 4px 8px; font-size: 14px;'>beta</span></span>
 					</a>
 				</div>
 
